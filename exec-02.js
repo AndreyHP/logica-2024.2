e5c9cff9e104e95089b31666f072f1c6e9b90269
Programador Full Stack 2024.2 <16133232024.2@senacrjedu.local>
2024-11-06 20:37:45 -0300

    exercicio 02

---
+++ exec-02.js
@@ -0,0 +1,18 @@
+//Escreva um programa para ler uma temperatura em graus Fahrenheit, calcular e exrever o valor correspondente em graus Celsius.
+// (C = (F - 32) * 5 /9)
+
+// Entrada
+
+let F = prompt("Digite o valor em Fahrenheit");
+
+//Processamento
+
+let C = (F - 32) *  (5/9);
+
+//saida
+
+alert("Temperatura em C: " + C);
+
+
+
+
