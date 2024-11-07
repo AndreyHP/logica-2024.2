54b8e4a2d506dafe005e121f04afdf37c9d19eef
Andrey Marins <16133232024.2@senacrjedu.local>
2024-11-06 21:15:41 -0300

    exercicio 03

---
+++ exec-03.js
@@ -0,0 +1,14 @@
+// Escreva um programa para ler uma temperatura em graus Celsius, e devolver o valor em Fahrenheit.
+//(F = (C * 9 / 5) + 32)
+
+// entrada
+
+let C = prompt("Digite o valor em Celsius");
+
+// Processamento
+
+let F = (C * 9 / 5) + 32;
+
+//Saida
+
+alert("Temperatura em F: " + F);
